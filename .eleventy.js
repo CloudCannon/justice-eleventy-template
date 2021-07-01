bd78b52bdcab2b1888f33e2ca9d71edc0d4aec71
Ross Phillips <rphillips-nz@users.noreply.github.com>
2021-07-01 15:10:26 +1200

    Formatting and configuration tidy up

--- .eleventy.js
+++ .eleventy.js
@@ -3,21 +3,21 @@ const pluginRss = require("@11ty/eleventy-plugin-rss");
 const seoData = require('./_data/seo.json');
 const socialIcons = require('./social-icons.js');
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
 	eleventyConfig.setUseGitIgnore(false);
 	eleventyConfig.setDataDeepMerge(true);
 
 	eleventyConfig.setFrontMatterParsingOptions({
-    excerpt: function (file, options) {
-		  file.excerpt = file.content.split('\n').slice(1, 2).join(' ');
+		excerpt: function (file, options) {
+			file.excerpt = file.content.split('\n').slice(1, 2).join(' ');
 		}
-  });
+	});
 
-  eleventyConfig.addCollection('nav', function (collectionApi) {
-    return collectionApi.getAll()
-    	.filter((page) => page.data.show_in_navigation === true)
-    	.sort((a, b) => a.data.navigation_order - b.data.navigation_order);
-  });
+	eleventyConfig.addCollection('nav', function (collectionApi) {
+		return collectionApi.getAll()
+			.filter((page) => page.data.show_in_navigation === true)
+			.sort((a, b) => a.data.navigation_order - b.data.navigation_order);
+	});
 
 	eleventyConfig.addShortcode('socialIcon', (icon) => socialIcons[icon] || '');
 
@@ -30,5 +30,11 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy('css');
 
 	eleventyConfig.addPlugin(pluginSeo, seoData);
-  eleventyConfig.addPlugin(pluginRss);
+	eleventyConfig.addPlugin(pluginRss);
+
+	eleventyConfig.cloudcannonOptions = {
+		dir: {
+			pages: 'pages'
+		}
+	};
 };
